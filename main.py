39ab39363d2ee9acf062701eba321caae213222d
pashudzu <166515168+pashudzu@users.noreply.github.com>
2025-05-27 11:06:03 +0300

    face detection

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import cv2 as cv
 
+face_cascade = cv.CascadeClassifier(cv.data.haarcascades + 'haarcascade_frontalface_default.xml')
+
 cam = cv.VideoCapture(0)
 
 if not cam.isOpened():
@@ -11,6 +13,10 @@ while True:
     if not result:
         print("Here no result")
         break
+    gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
+    faces = face_cascade.detectMultiScale(gray, 1.3, 5)
+    for (x, y, w, h) in faces:
+        cv.rectangle(frame, (x, y), (x + w, y + h), (0, 0, 255), 2)
     cv.imshow("VideoCapture", frame)
     if cv.waitKey(1) & 0xFF == ord('q'):
         print("Exit key just pressed")
